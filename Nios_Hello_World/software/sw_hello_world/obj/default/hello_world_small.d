f426be974b89ab63f9d0b56eee8a636999b73eca
aileneiioana <your_email_address@example.com>
2023-06-14 10:50:45 +0300

    Added test project

---
+++ Nios_Hello_World/software/sw_hello_world/obj/default/hello_world_small.d
@@ -0,0 +1,17 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c:/Users/ailen/Desktop/licenta/Nios_Hello_World/software/sw_hello_world_bsp/system.h \
+ c:/Users/ailen/Desktop/licenta/Nios_Hello_World/software/sw_hello_world_bsp/linker.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\drivers\inc\altera_avalon_pio_regs.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\io.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\alt_types.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\alt_types.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\sys\alt_irq.h \
+ c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\nios2.h
+c:/Users/ailen/Desktop/licenta/Nios_Hello_World/software/sw_hello_world_bsp/system.h:
+c:/Users/ailen/Desktop/licenta/Nios_Hello_World/software/sw_hello_world_bsp/linker.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\drivers\inc\altera_avalon_pio_regs.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\io.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\alt_types.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\alt_types.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\sys\alt_irq.h:
+c:\users\ailen\desktop\licenta\nios_hello_world\software\sw_hello_world_bsp\hal\inc\nios2.h:
